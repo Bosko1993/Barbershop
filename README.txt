e2f1e721563532da299928eaac55d747f128784b
Bosko1993 <boky.officialmail@gmail.com>
2026-02-15 16:18:08 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,15 @@
+Barbershop Termini PWA
+
+Ovo je PWA (instalira se kao aplikacija).
+
+Kako pokrenuti BESPLATNO:
+1) GitHub (Pages): napravi repo, uploaduj sve fajlove iz ovog foldera, uključi Pages (Deploy from branch).
+2) Cloudflare Pages: upload folder / poveži GitHub, Deploy.
+3) Netlify: drag&drop folder.
+
+Nakon što dobiješ link:
+- Otvori link na telefonu
+- Menu (⋮) → Add to Home screen
+
+Podaci se čuvaju lokalno (u telefonu) u localStorage.
+Export/Import služi za backup (JSON fajl).
---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = "barber-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((cache) => cache.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(()=> cached))
+  );
+});
